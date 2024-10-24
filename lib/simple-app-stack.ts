39f9fec9b906672c927d97e995eb0378becc069b
mustafe2889 <20088394@mail.wit.ie>
2024-10-24 22:09:44 +0100

    Lambda communicating with DynamoDB

--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -1,16 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambdanode from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';  // NEW IMPORT
-import * as custom from "aws-cdk-lib/custom-resources"; // ADD THIS IMPORT
-import { generateBatch } from "../shared/util"; // ADD THIS IMPORT
-import { movies } from "../seed/movies"; // ADD THIS IMPORT
-
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';  // Ensure this import is present
+import * as custom from 'aws-cdk-lib/custom-resources';
+import { generateBatch } from '../shared/util';
+import { movies } from '../seed/movies';
 import { Construct } from 'constructs';
 
 export class SimpleAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // Reference the existing DynamoDB table using only the ARN
+    const moviesTable = dynamodb.Table.fromTableAttributes(this, 'MoviesTable', {
+      tableArn: 'arn:aws:dynamodb:eu-west-1:515246745010:table/Movies', // Use the actual ARN of your existing table
+    });
 
     const simpleFn = new lambdanode.NodejsFunction(this, "SimpleFn", {
       architecture: lambda.Architecture.ARM_64,
@@ -21,7 +25,7 @@ export class SimpleAppStack extends cdk.Stack {
     });
 
     const simpleFnURL = simpleFn.addFunctionUrl({
-      authType: lambda.FunctionUrlAuthType.AWS_IAM,   // PREVIOUS CHANGE
+      authType: lambda.FunctionUrlAuthType.AWS_IAM,   // Keep previous change
       cors: {
         allowedOrigins: ["*"],
       },
@@ -29,30 +33,28 @@ export class SimpleAppStack extends cdk.Stack {
 
     new cdk.CfnOutput(this, "Simple Function Url", { value: simpleFnURL.url });
 
-    // NEW DynamoDB Table declaration
-    const moviesTable = new dynamodb.Table(this, "MoviesTable", {
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-      partitionKey: { name: "id", type: dynamodb.AttributeType.NUMBER },
-      removalPolicy: cdk.RemovalPolicy.DESTROY,  // Delete table when stack is destroyed
-      tableName: "NewMoviesTable",  // Change the name here
-    });
-    
-
-    // ADD AWSCustomResource to seed the DynamoDB table
-    new custom.AwsCustomResource(this, "moviesddbInitData", {
-      onCreate: {
-        service: "DynamoDB",
-        action: "batchWriteItem",
-        parameters: {
-          RequestItems: {
-            [moviesTable.tableName]: generateBatch(movies), // Use generateBatch to format movies data
-          },
-        },
-        physicalResourceId: custom.PhysicalResourceId.of("moviesddbInitData"),
+    // New Lambda Function
+    const getMovieByIdFn = new lambdanode.NodejsFunction(this, "GetMovieByIdFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      entry: `${__dirname}/../lambdas/getMovieById.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      environment: {
+        TABLE_NAME: moviesTable.tableName,
+        REGION: 'eu-west-1',
       },
-      policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
-        resources: [moviesTable.tableArn], // Provide permissions to access the table
-      }),
     });
+
+    const getMovieByIdURL = getMovieByIdFn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+      },
+    });
+
+    moviesTable.grantReadData(getMovieByIdFn);
+
+    new cdk.CfnOutput(this, "Get Movie Function Url", { value: getMovieByIdURL.url });
   }
 }
