3ef76083f1e27bdb0712790abf815375b2865127
mustafe2889 <20088394@mail.wit.ie>
2024-10-24 22:49:35 +0100

    Lambda function to get all movies.

--- lambdas/getAllMovies.ts
+++ lambdas/getAllMovies.ts
@@ -1,46 +1,23 @@
 import { Handler } from "aws-lambda";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
 
 // Initialization
 const ddbDocClient = createDDbDocClient();
 
 // Handler
-export const handler: Handler = async (event, context) => {
+export const handler: Handler = async (event) => {
   try {
-    console.log("Event: ", JSON.stringify(event));  // This can remain if you want basic logging
-    const parameters = event?.queryStringParameters;
-    const movieId = parameters ? parseInt(parameters.movieId) : undefined;
-
-    if (!movieId) {
-      return {
-        statusCode: 404,
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify({ Message: "Missing movie Id" }),
-      };
-    }
+    console.log("Event: ", JSON.stringify(event)); // Log the incoming event
 
     const commandOutput = await ddbDocClient.send(
-      new GetCommand({
-        TableName: process.env.TABLE_NAME,
-        Key: { id: movieId },
+      new ScanCommand({
+        TableName: process.env.TABLE_NAME, // Use your table name from the environment variable
       })
     );
 
-    if (!commandOutput.Item) {
-      return {
-        statusCode: 404,
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify({ Message: "Invalid movie Id" }),
-      };
-    }
-
     const body = {
-      data: commandOutput.Item,
+      data: commandOutput.Items, // Return the scanned items
     };
 
     // Return Response
@@ -52,7 +29,7 @@ export const handler: Handler = async (event, context) => {
       body: JSON.stringify(body),
     };
   } catch (error: any) {
-    console.log(JSON.stringify(error));  // You can keep this for error logging
+    console.log(JSON.stringify(error));
     return {
       statusCode: 500,
       headers: {
--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -2,9 +2,6 @@ import * as cdk from 'aws-cdk-lib';
 import * as lambdanode from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';  // Ensure this import is present
-import * as custom from 'aws-cdk-lib/custom-resources';
-import { generateBatch } from '../shared/util';
-import { movies } from '../seed/movies';
 import { Construct } from 'constructs';
 
 export class SimpleAppStack extends cdk.Stack {
@@ -16,6 +13,7 @@ export class SimpleAppStack extends cdk.Stack {
       tableArn: 'arn:aws:dynamodb:eu-west-1:515246745010:table/Movies', // Use the actual ARN of your existing table
     });
 
+    // Existing simpleFn Lambda
     const simpleFn = new lambdanode.NodejsFunction(this, "SimpleFn", {
       architecture: lambda.Architecture.ARM_64,
       runtime: lambda.Runtime.NODEJS_18_X,
@@ -33,7 +31,7 @@ export class SimpleAppStack extends cdk.Stack {
 
     new cdk.CfnOutput(this, "Simple Function Url", { value: simpleFnURL.url });
 
-    // New Lambda Function
+    // GetMovieById Lambda Function
     const getMovieByIdFn = new lambdanode.NodejsFunction(this, "GetMovieByIdFn", {
       architecture: lambda.Architecture.ARM_64,
       runtime: lambda.Runtime.NODEJS_18_X,
@@ -56,5 +54,29 @@ export class SimpleAppStack extends cdk.Stack {
     moviesTable.grantReadData(getMovieByIdFn);
 
     new cdk.CfnOutput(this, "Get Movie Function Url", { value: getMovieByIdURL.url });
+
+    // New getAllMovies Lambda Function
+    const getAllMoviesFn = new lambdanode.NodejsFunction(this, "GetAllMoviesFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      entry: `${__dirname}/../lambdas/getAllMovies.ts`,  // The new Lambda function file
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      environment: {
+        TABLE_NAME: moviesTable.tableName,
+        REGION: 'eu-west-1',
+      },
+    });
+
+    const getAllMoviesURL = getAllMoviesFn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+      },
+    });
+
+    moviesTable.grantReadData(getAllMoviesFn);  // Grant permission to read data from the table
+
+    new cdk.CfnOutput(this, "Get All Movies Function Url", { value: getAllMoviesURL.url });
   }
 }
