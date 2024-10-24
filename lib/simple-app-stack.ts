2cd5770033f5840d379292bc423f4d5262e10ab9
mustafe2889 <20088394@mail.wit.ie>
2024-10-24 21:53:49 +0100

    Create movies table and seed it.

--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -1,6 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambdanode from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';  // NEW IMPORT
+import * as custom from "aws-cdk-lib/custom-resources"; // ADD THIS IMPORT
+import { generateBatch } from "../shared/util"; // ADD THIS IMPORT
+import { movies } from "../seed/movies"; // ADD THIS IMPORT
 
 import { Construct } from 'constructs';
 
@@ -17,12 +21,38 @@ export class SimpleAppStack extends cdk.Stack {
     });
 
     const simpleFnURL = simpleFn.addFunctionUrl({
-      authType: lambda.FunctionUrlAuthType.AWS_IAM,   // UPDATED HERE
+      authType: lambda.FunctionUrlAuthType.AWS_IAM,   // PREVIOUS CHANGE
       cors: {
         allowedOrigins: ["*"],
       },
     });
 
     new cdk.CfnOutput(this, "Simple Function Url", { value: simpleFnURL.url });
+
+    // NEW DynamoDB Table declaration
+    const moviesTable = new dynamodb.Table(this, "MoviesTable", {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: "id", type: dynamodb.AttributeType.NUMBER },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,  // Delete table when stack is destroyed
+      tableName: "NewMoviesTable",  // Change the name here
+    });
+    
+
+    // ADD AWSCustomResource to seed the DynamoDB table
+    new custom.AwsCustomResource(this, "moviesddbInitData", {
+      onCreate: {
+        service: "DynamoDB",
+        action: "batchWriteItem",
+        parameters: {
+          RequestItems: {
+            [moviesTable.tableName]: generateBatch(movies), // Use generateBatch to format movies data
+          },
+        },
+        physicalResourceId: custom.PhysicalResourceId.of("moviesddbInitData"),
+      },
+      policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
+        resources: [moviesTable.tableArn], // Provide permissions to access the table
+      }),
+    });
   }
 }
